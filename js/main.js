db48030b9b635e7b3208982bffcc03eb0106c313
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-08 20:53:29 +0100

    Search series by name and show results on scree. Set image medium format and default image.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 'use strict';
+
 // global variables:
 const btnElement = document.querySelector('.js-form_btn');
-const urlAPI = "http://api.tvmaze.com/shows";
+const searchUrlAPI = "http://api.tvmaze.com/search/shows?q=";
+const defaultImageHTML = '<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
 
 // btn prevent default:
-document.querySelector(".js-form_btn").addEventListener("click",
-    function (event) {
-    event.preventDefault()
-});
+function resetBehaviorsButton() {
+    document.querySelector(".js-form_btn").addEventListener("click",
+        function (event) {
+            event.preventDefault()
+        });
+}
+
+// Event listeners
+
+function addEventSearcher() { 
+  btnElement.addEventListener("click", inputSearchValue);
+}
 
 // functions:
 
-function getApiInformation() {
-    fetch(urlAPI)
+function searchShows(search) {
+    fetch(searchUrlAPI+search)
     .then(response => { return response.json(); })
-    .then(data => {
+        .then(shows => {
+        //Esto puede ser una funcion apartada:
         let html = '';
-        for (let i = 0; i < data.length; i++){
+        for (const show of shows){
             html +=  '<li>'
-            html += `<p>${data[i].name}</p>`;
-            html += `<img src="${data[i].image.medium}" />`;
+            html += `<p>${show.show.name}</p>`;
+            html += imageHTML(show);
             html +=  '</li>'
         }
         const list = document.querySelector('.js-search_list');
@@ -27,13 +38,27 @@ function getApiInformation() {
     })
 }
 
+function imageHTML(show) {
+    let imageHTML = defaultImageHTML;
+    if (show.show.image != null)
+      imageHTML = `<img src="${show.show.image.medium}" />`;
+
+    return imageHTML;
+}
+
 function inputSearchValue() {
     const search = document.querySelector('.js-input').value;
     if (search === '') {
         return console.log("vazio");
     } else {
-        return console.log(search);
+        searchShows(search);
     }
 }
-btnElement.addEventListener("click", inputSearchValue);
-getApiInformation();
+
+function init() {
+    resetBehaviorsButton();
+    addEventSearcher();
+
+}
+
+init();
