9fff8cd82b8be9389a805841cfd92b4394ebe435
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-08 22:55:14 +0100

    Set favourites items function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// global variables:
+// global variables: //
 const btnElement = document.querySelector('.js-form_btn');
 const searchUrlAPI = "http://api.tvmaze.com/search/shows?q=";
-const defaultImageHTML = '<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
+const defaultImageHTML = '<img class="search_img" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
 
 // btn prevent default:
 function resetBehaviorsButton() {
@@ -13,13 +13,23 @@ function resetBehaviorsButton() {
         });
 }
 
-// Event listeners
+// Event listeners //
 
 function addEventSearcher() { 
   btnElement.addEventListener("click", inputSearchValue);
 }
 
-// functions:
+function addEventFavorites() {
+    const resultItems = document.querySelectorAll(".search_item");
+    for (const item of resultItems) {
+       item.addEventListener("click",
+        function (event) {
+            event.currentTarget.classList.toggle("js-favorite");
+        });
+    }  
+}
+
+// functions: //
 
 function searchShows(search) {
     fetch(searchUrlAPI+search)
@@ -28,37 +38,35 @@ function searchShows(search) {
         //Esto puede ser una funcion apartada:
         let html = '';
         for (const show of shows){
-            html +=  '<li>'
-            html += `<p>${show.show.name}</p>`;
+            html +=  '<li class="search_item">'
+            html += `<p class="search_item-title">${show.show.name}</p>`;
             html += imageHTML(show);
             html +=  '</li>'
         }
         const list = document.querySelector('.js-search_list');
-        list.innerHTML = html;
+        list.innerHTML = html; 
+        addEventFavorites();
     })
 }
 
 function imageHTML(show) {
     let imageHTML = defaultImageHTML;
     if (show.show.image != null)
-      imageHTML = `<img src="${show.show.image.medium}" />`;
+      imageHTML = `<img class="search_img" src="${show.show.image.medium}" />`;
 
     return imageHTML;
 }
 
 function inputSearchValue() {
     const search = document.querySelector('.js-input').value;
-    if (search === '') {
-        return console.log("vazio");
-    } else {
+    if (search !== '') {
         searchShows(search);
-    }
+    } 
 }
 
 function init() {
     resetBehaviorsButton();
     addEventSearcher();
-
 }
 
 init();
