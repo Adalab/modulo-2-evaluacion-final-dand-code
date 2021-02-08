1f10e77dafb83b24d6c6132a59d822c988907460
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-08 18:37:42 +0100

    Add image url with API information

--- js/main.js
+++ js/main.js
@@ -18,17 +18,14 @@ function getApiInformation() {
         let html = '';
         for (let i = 0; i < data.length; i++){
             html +=  '<li>'
-            html += data[i].name;
+            html += `<p>${data[i].name}</p>`;
+            html += `<img src="${data[i].image.medium}" />`;
             html +=  '</li>'
         }
         const list = document.querySelector('.js-search_list');
         list.innerHTML = html;
     })
 }
-
-
-   
-
 
 function inputSearchValue() {
     const search = document.querySelector('.js-input').value;
@@ -40,39 +37,3 @@ function inputSearchValue() {
 }
 btnElement.addEventListener("click", inputSearchValue);
 getApiInformation();
-
-/*
-const btnElement = document.querySelector('.js-form_btn');
-const listElement = document.querySelector('.js-search_list');
-const urlAPI = 'http://api.tvmaze.com/shows';
-
-let allSeries = [];
-let favoriteSeries = [];
-
-function getApiInformation(evt) {
-    console.log(evt);
-    fetch(urlAPI)
-    .then((response) => {
-        return response.json();
-      })
-    .then((data) => {
-        console.log(data[1].show);
-        paintListHTML(evt);
-    });
-    paintListHTML();
-}
-getApiInformation();
-
-function paintListHTML(evt) {
-    let html = '';
-    for(let i = 0; i < allSeries.length; i++){
-        html += '<li class="item">';
-        html += allSeries[i];
-        html += '</li>';
-    }
-    evt.preventDefault();
-    listElement.innerHTML = html;
-} 
-
-btnElement.addEventListener("click", getApiInformation);
-*/
