9da7e752b0871f4cd194955ddc668c467ced6a7b
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-10 13:13:12 +0100

    Add searchItems and favouriteItems arrays

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const btnReset = document.querySelector('.reset');
 const searchUrlAPI = "//api.tvmaze.com/search/shows?q=";
 const defaultImageHTML = '<img class="search_img" src="//via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
 
-
+//Arrays
+let searchItems = [];
+let favouriteItems = [];
 
 // EVENTS //
 
@@ -21,7 +23,7 @@ function addEventFavourites() {
         function (event) {
             event.currentTarget.classList.toggle("js-favourite");
             if (event.currentTarget.classList.contains("js-favourite")) {
-                addToFavourites(item.querySelector('p').innerText, item.innerHTML);
+                addToFavourites(item.querySelector('p').innerText);
             } else {
                 removeFromFavourites(item.querySelector('p').innerText);
             }
@@ -40,7 +42,7 @@ function resetBehaviorsButton() {
 // USER BEHAVIOR - FAVOURITES//
 
 function clearFavourites() {
-    localStorage.setItem("favourites", JSON.stringify({}));
+    favouriteItems = [];
     refreshFavoutites();  
 }
 
@@ -51,18 +53,14 @@ function eliminateFavorite() {
 }
 
 function removeFromFavourites(name) { 
-    let allFavourites = JSON.parse(localStorage.getItem("favourites"));
-    delete allFavourites[name];
-    localStorage.setItem("favourites", JSON.stringify(allFavourites));
+    favouriteItems = favouriteItems.filter(item => item.show.name != name);
 }
 
 function addToFavourites(name, showHTML) { 
-    let allFavourites = JSON.parse(localStorage.getItem("favourites"));
-    if (allFavourites == null) {
-        allFavourites = {};
+    const newFavourite = searchItems.filter(item => item.show.name == name);
+    if (newFavourite.length !== 0) {
+        favouriteItems.push(newFavourite[0]);
     }
-    allFavourites[name] = showHTML;
-    localStorage.setItem("favourites", JSON.stringify(allFavourites));
 }
 
 // SETTING LOCAL STORAGE //
@@ -74,36 +72,43 @@ function setLocalStorage() {
 
 // HTML CODE //
 function refreshFavoutites() {
-    const allFavourites = JSON.parse(localStorage.getItem("favourites"));
     const listFavourite = document.querySelector('.js-favourites_list');
     let favouritesHTML = "";
-    for (const favourite in allFavourites) { 
+    for (const favourite of favouriteItems) { 
         favouritesHTML +=  '<li class="favourite_item">';
-        favouritesHTML += allFavourites[favourite];
+        favouritesHTML += `<p class="search_item-title">${favourite.show.name}</p>`;
+        favouritesHTML += imageHTML(favourite);
         favouritesHTML +=  '</li>';
     }
     listFavourite.innerHTML = favouritesHTML; 
-    const favouritesItems = listFavourite.querySelectorAll(".favourite_item");
-    for (const favouriteHTML of favouritesItems) {
+
+    addEventFavouritesItems();
+};
+
+function addEventFavouritesItems() { 
+    const favouriteItemsHTML = document.querySelectorAll(".favourite_item");
+    for (const favouriteHTML of favouriteItemsHTML) {
       favouriteHTML.addEventListener("click", () => {
         removeFromFavourites(favouriteHTML.querySelector('p').innerText);
         refreshFavoutites();
       });
     }
-};
-
+}
 
 function serieHTML(data) {
+    searchItems = [];
     let html = '';
-    for (const item of data){
+    for (const item of data) {
         html +=  '<li class="search_item">'
         html += `<p class="search_item-title">${item.show.name}</p>`;
         html += imageHTML(item);
         html += '</li>';
+        searchItems.push(item);
     }
     const list = document.querySelector('.js-search_list');
     list.innerHTML = html; 
 }
+
 function imageHTML(item) {
     let imageHTML = defaultImageHTML;
     if (item.show.image != null)
