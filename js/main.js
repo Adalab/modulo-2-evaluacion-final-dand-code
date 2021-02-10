bb06d4d87e7195ba89aba930d0ec4e971237afc0
Dandara Almeida <dandara.avazquez@gmail.com>
2021-02-10 12:30:27 +0100

    Fix http API

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// global variables: //
+// GLOBAL VARIABLES //
 const btnElement = document.querySelector('.js-form_btn');
-const searchUrlAPI = "http://api.tvmaze.com/search/shows?q=";
-const defaultImageHTML = '<img class="search_img" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
+const btnReset = document.querySelector('.reset');
+const searchUrlAPI = "//api.tvmaze.com/search/shows?q=";
+const defaultImageHTML = '<img class="search_img" src="//via.placeholder.com/210x295/ffffff/666666/?text=TV" />';
 
-// array //
-let allFavourites = {};
 
-// Event listeners //
+
+// EVENTS //
 
 function addEventSearcher() { 
   btnElement.addEventListener("click", inputSearchValue);
@@ -30,13 +30,30 @@ function addEventFavourites() {
     }
 }  
 
-// functions: //
+function resetBehaviorsButton() {
+    document.querySelector(".js-form_btn").addEventListener("click",
+        function (event) {
+            event.preventDefault()
+        });
+}
+
+// USER BEHAVIOR - FAVOURITES//
+
+function clearFavourites() {
+    localStorage.setItem("favourites", JSON.stringify({}));
+    refreshFavoutites();  
+}
+
 function eliminateFavorite() {
     allFavourites.addEventListener("click", function (event) {
         event.currentTarget.classList.remove("js-favourite");
-    });
-   
-        
+    });    
+}
+
+function removeFromFavourites(name) { 
+    let allFavourites = JSON.parse(localStorage.getItem("favourites"));
+    delete allFavourites[name];
+    localStorage.setItem("favourites", JSON.stringify(allFavourites));
 }
 
 function addToFavourites(name, showHTML) { 
@@ -48,50 +65,45 @@ function addToFavourites(name, showHTML) {
     localStorage.setItem("favourites", JSON.stringify(allFavourites));
 }
 
-function removeFromFavourites(name) { 
-    let allFavourites = JSON.parse(localStorage.getItem("favourites"));
-    delete allFavourites[name];
-    localStorage.setItem("favourites", JSON.stringify(allFavourites));
-}
-
-function resetBehaviorsButton() {
-    document.querySelector(".js-form_btn").addEventListener("click",
-        function (event) {
-            event.preventDefault()
-        });
-}
-function refreshFavoutites() {
-    const allFavourites = JSON.parse(localStorage.getItem("favourites"));
-    const listFavourite = document.querySelector('.js-favourites_list');
-    listFavourite.innerHTML = "";
-    for (const favourite in allFavourites) { 
-        listFavourite.innerHTML += allFavourites[favourite];
-    }   
-};
+// SETTING LOCAL STORAGE //
 
 function setLocalStorage() {
     localStorage.setItem('allFavourites', JSON.stringify(allFavourites));
 }
 
 
-function searchShows(search) {
-    fetch(searchUrlAPI+search)
-    .then(response => { return response.json(); })
-        .then(data => {
-        //Esto puede ser una funcion apartada:
-        let html = '';
-        for (const item of data){
-            html +=  '<li class="search_item">'
-            html += `<p class="search_item-title">${item.show.name}</p>`;
-            html += imageHTML(item);
-            html +=  '</li>'
-        }
-        const list = document.querySelector('.js-search_list');
-        list.innerHTML = html; 
-        addEventFavourites();
-    })
-}
+// HTML CODE //
+function refreshFavoutites() {
+    const allFavourites = JSON.parse(localStorage.getItem("favourites"));
+    const listFavourite = document.querySelector('.js-favourites_list');
+    let favouritesHTML = "";
+    for (const favourite in allFavourites) { 
+        favouritesHTML +=  '<li class="favourite_item">';
+        favouritesHTML += allFavourites[favourite];
+        favouritesHTML +=  '</li>';
+    }
+    listFavourite.innerHTML = favouritesHTML; 
+    const favouritesItems = listFavourite.querySelectorAll(".favourite_item");
+    for (const favouriteHTML of favouritesItems) {
+      favouriteHTML.addEventListener("click", () => {
+        removeFromFavourites(favouriteHTML.querySelector('p').innerText);
+        refreshFavoutites();
+      });
+    }
+};
 
+
+function serieHTML(data) {
+    let html = '';
+    for (const item of data){
+        html +=  '<li class="search_item">'
+        html += `<p class="search_item-title">${item.show.name}</p>`;
+        html += imageHTML(item);
+        html += '</li>';
+    }
+    const list = document.querySelector('.js-search_list');
+    list.innerHTML = html; 
+}
 function imageHTML(item) {
     let imageHTML = defaultImageHTML;
     if (item.show.image != null)
@@ -100,6 +112,19 @@ function imageHTML(item) {
     return imageHTML;
 }
 
+// COMUNICATION WITH API //
+function searchShows(search) {
+    fetch(searchUrlAPI+search)
+        .then(response => { return response.json(); })
+        .then(data => {
+        serieHTML(data);
+        addEventFavourites();
+    })
+}
+
+
+// SEARCH INPUT //
+
 function inputSearchValue() {
     const search = document.querySelector('.js-input').value;
     if (search !== '') {
@@ -107,10 +132,13 @@ function inputSearchValue() {
     } 
 }
 
+// Init aplication// 
 function init() {
     resetBehaviorsButton();
     addEventSearcher();
     refreshFavoutites();
+
+    btnReset.addEventListener("click", clearFavourites);
 }
 
 init();
